16f6a6e151d2fc1f6a30728bf4d348c6df5522bc
julietazotes <julietaazotes@gmail.com>
2025-05-19 20:24:19 -0300

    ajuste import Home para Render

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Productos from './pages/Productos';
 import Nosotros from './pages/Nosotros';
 import Contacto from './pages/Contacto';
